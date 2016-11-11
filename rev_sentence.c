4ac0b6f051be0e3df22a434f1e5e95e0a75604b7
Jaydeep Ingle <jingle1@binghamton.edu>
2016-11-11 01:53:48 -0500

    miscallaneous changes

--- rev_sentence.c
+++ rev_sentence.c
@@ -1,45 +1,36 @@
-
 #include <stdio.h>
 #include <string.h>
 #define MAX 128
 int main()
 {
-	char a[MAX] = "I am from COEP";
-	int i, j, k;
-	printf("%s\n", a);
-//	printf("Enter Sentence: ");
-//	scanf("%s", a);
-//	gets(a);
-	int len = strlen(a);
-	char tmp;
-	for(i = 0, j = len - 1; i < (len / 2); i++, j--)
-	{
-		tmp = a[i];
-		a[i] =  a[j];
-		a[j] = tmp; 
-	}
-	int count = 0;
-	int int_temp;
-	for(i = 0; i < len; i++)
-	{
-		count++;
-		if(a[i] == ' ')
-		{
-// 			int_temp = i;
-			for(j = 0, k = count; j < (count / 2); j++, k--)
-			{
-				tmp = a[j];
-				a[j] = a[k];
-				a[k] = tmp;
-			}
-			count = 0;
-			
-		}
-		printf("%d  ", i);
-		
-//		i = int_temp;
-		
-	}
-	printf("\n%s\n", a);
-	return 0;
+    char a[MAX] = "I am from COEP";
+    int i, j, k;
+    printf("%s\n", a);
+    int len = strlen(a);
+    char tmp;
+    for(i = 0, j = len - 1; i < (len / 2); i++, j--)
+    {
+        tmp = a[i];
+        a[i] =  a[j];
+        a[j] = tmp; 
+    }
+    int count = 0;
+    int int_temp;
+    for(i = 0; i < len; i++)
+    {
+        count++;
+        if(a[i] == ' ')
+        {
+            for(j = 0, k = count; j < (count / 2); j++, k--)
+            {
+                tmp = a[j];
+                a[j] = a[k];
+                a[k] = tmp;
+            }
+            count = 0;
+        }
+        printf("%d  ", i);
+    }
+    printf("\n%s\n", a);
+    return 0;
 }
--- spiral_mat.c
+++ spiral_mat.c
@@ -1,90 +1,82 @@
-//This is program for spiral matrix. Spiral Matrix is a matrix with
-//the middle element as 0 and will be incremented by 1 as it goes out spirally
-//eg: n=3
-//           4 3 2
-//           5 0 1
-//           6 7 8
- 
- 
 #include<iostream>
 #include<cstdlib>
 #include<iomanip>
 using namespace std;
 int n;
- 
+
 //returns the direction in which the next element to be filled up in the matrix
 int direction(int i, int j, int key)
-   {
-	if((abs((n/2)-i)==key) && (abs((n/2)-j)==key))
-	{
- 	if(i<(n/2) && j>(n/2))
-     	   return 1;//left
- 	else if(i<(n/2) && j<(n/2))
-   	     return 2;//down
-	 else if(i>(n/2) && j<(n/2))
-     	   return 3;//right
- 	else return -1;
-   	 }
+{
+    if((abs((n/2)-i)==key) && (abs((n/2)-j)==key))
+    {
+        if(i<(n/2) && j>(n/2))
+            return 1;
+        else if(i<(n/2) && j<(n/2))
+            return 2;
+        else if(i>(n/2) && j<(n/2))
+            return 3;
+        else return -1;
+    }
     else if((abs((n/2)-i)==key) && (abs((n/2)-j)>key) && i>(n/2) && j>(n/2))
-      return 0;//up
- 
+        return 0;//up
+
     else return -1;
-   }
- 
+}
+
 int main(){
     int i,j,count=1,temp;
     cout<<"Enter n (<50): ";
     cin>>n;
-     if(n%2==0)
-     n++; //making n odd
-int sp[50][50],key,dir;
- 
-for(i=0;i<n;i++)
-for(j=0;j<n;j++)
-sp[i][j]=0;
-key=1;
-i=n/2;
-j=(n/2)+1;
-temp=0;
- 
-while(true)
-{
-//giving values in the array
-   sp[i][j]=count;       
- if(temp!=-1)
-   dir=temp;
-      switch(dir){
- 
-      case 0://up
-           i--;
-           break;
-      case 1://left
-           j--;
-           break;
-      case 2://down
-           i++;
-           break;
-      case 3://right
-           j++;
-           break;
-      default:
-              break;
-              }
- 
-   if(key>n || i<0 || j<0)
-   break;
-//getting the direction
-   temp = direction(i,j,key);
-   if((j-(n/2))>key)
-   key++;
-   count++;
-}
- 
-//outputing
-for(i=0;i<n;i++){
-for(j=0;j<n;j++)
-cout<<std::setw(4)<<sp[i][j];
-cout<<"\n";}
- 
-return 0;
+    if(n%2==0)
+        n++; //making n odd
+    int sp[50][50],key,dir;
+
+    for(i=0;i<n;i++)
+        for(j=0;j<n;j++)
+            sp[i][j]=0;
+    key=1;
+    i=n/2;
+    j=(n/2)+1;
+    temp=0;
+
+    while(true)
+    {
+        //giving values in the array
+        sp[i][j]=count;       
+        if(temp!=-1)
+            dir=temp;
+        switch(dir){
+
+            case 0://up
+                i--;
+                break;
+            case 1://left
+                j--;
+                break;
+            case 2://down
+                i++;
+                break;
+            case 3://right
+                j++;
+                break;
+            default:
+                break;
+        }
+
+        if(key>n || i<0 || j<0)
+            break;
+        //getting the direction
+        temp = direction(i,j,key);
+        if((j-(n/2))>key)
+            key++;
+        count++;
+    }
+
+    //outputing
+    for(i=0;i<n;i++){
+        for(j=0;j<n;j++)
+            cout<<std::setw(4)<<sp[i][j];
+        cout<<"\n";}
+
+    return 0;
 }
